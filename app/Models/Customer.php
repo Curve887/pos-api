46c730454c5ca1cd22d01a0202dca970970a9058
ade <kurniawanadeputra17@gmail.com>
2025-10-13 22:54:57 +0700

    [ADD] API untuk Customer On Progress

---
+++ app/Models/Customer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Customer extends Authenticatable
+{
+    use HasApiTokens, HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone',
+        'address',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
